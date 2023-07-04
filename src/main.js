07c77b24e102abb9ef818057a65339eb77e0302c
bisudal123 <bisu9175@naver.com>
2023-07-04 16:20:45 +0900

    java-header

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+
+
+
+document.addEventListener('scroll',  () => {
+
+console.log(window.scrollY);
+
+if(window.scrollY > headerHeight) {
+    header.classList.add('header--dark');
+}
+else {
+    header.classList.remove('header--dark')
+}
+
+});
