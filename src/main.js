554f57e25cd08611f2be140bb788d6f0b4bd566a
bisudal123 <bisu9175@naver.com>
2023-07-05 15:06:54 +0900

    js-home

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 
 
 
 document.addEventListener('scroll',  () => {
 
-console.log(window.scrollY);
+
 
 if(window.scrollY > headerHeight) {
     header.classList.add('header--dark');
@@ -14,4 +14,15 @@ else {
     header.classList.remove('header--dark')
 }
 
+});
+
+
+// Home 섹션을 아래로 스크롤시 투명하게
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+    console.log( 1 - window.scrollY / homeHeight);
+
+home.style.opacity = 1 - window.scrollY / homeHeight;
 });
